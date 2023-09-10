01ccbaaaa7af4a2632dc45bc8cd4832a015d6a3f
Garpirik <ggarpir@gmail.com>
2023-09-11 00:37:28 +0200

    added new button

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from '../features/counter/counterSlice';
 import './App.scss'
@@ -13,6 +14,7 @@ function App() {
       <h1>Counter: {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <Button sx={{fontSize: "16px"}}>Text</Button>  
     </div>
   );
 }
