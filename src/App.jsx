95de82b9360c923ad4a6bc52f7a55978bd4bc130
Yuriy Putsan <putsan57@gmail.com>
2023-09-10 16:55:53 +0300

    base folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from '../features/counter/counterSlice';
+import { increment, decrement } from './modules/counter/counterSlice';
 import './App.scss'
 
 function App() {
