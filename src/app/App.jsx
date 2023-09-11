976bd74e1456f9712ab86cb5492953e7dcfe58ae
Yuriy Putsan <putsan57@gmail.com>
2023-09-11 21:50:27 +0300

    refactor: simplify files structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from './modules/counter/counterSlice';
+import { increment, decrement } from './store/counter/counterSlice';
 import './App.scss'
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <h1>Counter: {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <Button sx={{fontSize: "16px"}}>Text</Button>  
+      <Button sx={{fontSize: "16px"}}>Text</Button>
     </div>
   );
 }
