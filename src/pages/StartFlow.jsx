789b4a90580381ee75e43fb7726bbc1b55d62135
Yuriy Putsan <putsan57@gmail.com>
2023-09-12 09:33:39 +0300

    feat: add react-router and create first screen

--- src/pages/StartFlow.jsx
+++ src/pages/StartFlow.jsx
@@ -1,6 +1,57 @@
+import { Button } from "@mui/material"
+import { Link } from "react-router-dom";
+
 function StartFlow() {
   return (
-    <div>StartFlow</div>
-  )
+    <div>
+      <div>
+        <h1>LDIS</h1>
+        <p>Привіт, готовий до найкращого спілкування в своєму житті?</p>
+      </div>
+
+      <div style={{ margin: "55px 0 92px" }}>
+        <h3>
+          Реєстрація не потрібна!
+          <br />
+          Залітай і починай{" "}
+        </h3>
+
+        <Button
+        variant="contained"
+        sx={{
+          width: '140px',
+          height: '140px',
+          borderRadius: '50%',
+          marginRight: '10px'
+        }}
+      >
+        Обрати <br />чат
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          width: '140px',
+          height: '140px',
+          borderRadius: '50%'
+        }}
+      >
+        Здивуй <br />мене
+      </Button>
+      </div>
+
+      <div>
+      <Link to="/auth" style={{ marginRight: '25px' }}>
+        <Button variant="contained">
+          Реєстрація
+        </Button>
+      </Link>
+      <Link to="/auth">
+        <Button variant="contained">
+          Вхід
+        </Button>
+      </Link>
+    </div>
+    </div>
+  );
 }
 export default StartFlow
