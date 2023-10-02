4cdb8ac686457eccde6eed60126e53945b6be289
Garpirik <ggarpir@gmail.com>
2023-10-02 19:02:17 +0200

    Header completed add schema Main

--- src/components/HeaderLobby/HeaderLobby.jsx
+++ src/components/HeaderLobby/HeaderLobby.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import HeaderLobbyIcons from "../HeaderLobbyIcons/HeaderLobbyIcons"
 import HeaderLobbyText from "../HeaderLobbyText/HeaderLobbyText";
 
@@ -6,6 +7,37 @@ const HeaderLobby =() =>{
         <div>
         <HeaderLobbyIcons/>
         <HeaderLobbyText />
+        
+     <div style={{ margin: "55px 0 92px" }}>
+
+
+     <Button
+
+       variant="contained"
+       sx={{
+         width: "140px",
+         height: "140px",
+         borderRadius: "50%",
+         marginRight: "10px",
+       }}
+     >
+      
+       Здивуй <br />
+       мене
+     </Button>
+
+     <Button
+
+       variant="contained"
+       sx={{
+         width: "140px",
+         height: "140px",
+         borderRadius: "50%",
+       }}
+     >
+     Створити  тему
+     </Button>
+   </div>
         </div>
     )
 
