74d2080c52192cbd3d35384081b6d24604ee74a0
Garpirik <ggarpir@gmail.com>
2023-10-03 20:30:08 +0200

    Added Main Lobby, hardcoding image

---
+++ src/components/MainLobby/MainLobby.jsx
@@ -0,0 +1,38 @@
+import { Grid } from "@mui/material";
+
+
+const MainLobby = () =>{
+
+        return(
+            <div> 
+                
+                <div style={{textAlign:"left"}}> <strong> Найпопулярніші теми </strong> </div>
+                <Grid container spacing={2}>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" />  </Grid>
+                <Grid item xs={4}>  <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /> </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /> </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /> </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /> </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /> </Grid>
+                <Grid item xs={4}> <img src="https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg" /></Grid>
+                </Grid>
+                </div>
+        )
+}
+export default MainLobby;
--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,8 +1,10 @@
 import HeaderLobby from "../components/HeaderLobby/HeaderLobby";
+import MainLobby from "../components/MainLobby/MainLobby";
 const Lobby = () =>{
 return(
     <div> 
     <HeaderLobby />
+    <MainLobby/>
     </div>
    
 )
