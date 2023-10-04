208063bce87c5b416accdb0ca3572cc6b4738d4e
Yuriy Putsan <putsan57@gmail.com>
2023-10-05 00:12:47 +0300

    feat: connect the lobby screen to modal flow

--- frontend/src/components/GetNameModal/GetNameModal.jsx
+++ frontend/src/components/GetNameModal/GetNameModal.jsx
@@ -1,26 +1,25 @@
 import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./GetNameModal.scss";
+import { MODAL_CHOOSE } from "../../app/constants";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 310,
-  bgcolor: '#808080',
-  border: '2px solid #000',
+  bgcolor: "#808080",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 2,
 };
 
-export const GetNameModal = ({ handleClose }) => {
+const GetNameModal = ({ handleClose, flow }) => {
+  const isChooseFlow = flow === MODAL_CHOOSE;
 
   return (
-    <Modal
-      open={true}
-      onClose={handleClose}
-    >
+    <Modal open onClose={handleClose}>
       <Box sx={style}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           Як можна до тебе звертатись?
@@ -28,12 +27,14 @@ export const GetNameModal = ({ handleClose }) => {
 
         <TextField helperText="Це ім’я бачитимуть усі користувачі чату" />
 
-        <Link to="/chat/000">
+        <Link to={(isChooseFlow && "/lobby") || "/chat/000"}>
           <Button variant="contained" onClick={handleClose}>
-            Здивуй мене
+            {(isChooseFlow && "Здивуй мене") || "До спілкування!"}
           </Button>
         </Link>
       </Box>
     </Modal>
   );
 };
+
+export default GetNameModal;
