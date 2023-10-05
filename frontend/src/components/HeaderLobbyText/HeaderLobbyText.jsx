df3ed5fb33a10137104559ee927d830b241dd21c
Garpirik <ggarpir@gmail.com>
2023-10-05 23:42:51 +0200

    Test

--- frontend/src/components/HeaderLobbyText/HeaderLobbyText.jsx
+++ frontend/src/components/HeaderLobbyText/HeaderLobbyText.jsx
@@ -1,12 +1,14 @@
 import SearchField from "../SearchField/SearchField";
 
-const HeaderLobbyText =() =>{
-    return(
-        <div> <strong> Привіт, username!</strong> <div> Про що хочеш поговорити сьогодні?</div> 
-        <SearchField/>
-        </div>
-    )
-
-}
+const HeaderLobbyText = () => {
+  return (
+    <div>
+      {" "}
+      <strong> Привіт, username!</strong>{" "}
+      <div> Про що хочеш поговорити сьогодні?:) </div>
+      <SearchField />
+    </div>
+  );
+};
 
 export default HeaderLobbyText;
