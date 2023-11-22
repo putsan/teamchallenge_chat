8879a04d1d0a881c955206a18a869d0617658a73
Yuriy Putsan <putsan57@gmail.com>
2023-11-23 00:05:10 +0200

    style: finish the title and main image on 1st page

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,9 +3,13 @@ import { Provider } from "react-redux";
 import store from "./app/store/store";
 import App from "./app/App";
 import "./index.scss";
+import { applyPrimaryColor } from "./theme";
+import palette from "./theme/palette";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>,
 );
+
+applyPrimaryColor(palette.primary.main);
--- frontend/src/pages/StartFlow/StartFlow.jsx
+++ frontend/src/pages/StartFlow/StartFlow.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Typography, useTheme } from "@mui/material";
 import { useState } from "react";
 
 import GetNameModal from "../../components/GetNameModal/GetNameModal";
 import MoreInfoButton from "../../components/MoreInfoButton";
+import HappyPeople from "../../assets/1st_screen_pic.png";
 import { MODAL_MODES } from "../../app/constants";
 
 import "./StartFlow.scss";
@@ -11,6 +12,7 @@ import "./StartFlow.scss";
 function StartFlow() {
   const [showModal, setShowModal] = useState(false);
   const [flow, setFlow] = useState(null);
+  const theme = useTheme();
 
   const toShowModal = (flowType) => {
     setFlow(flowType);
@@ -19,12 +21,21 @@ function StartFlow() {
 
   return (
     <div className="start-flow">
-      <div className="start-flow__main" />
+      <div className="start-flow__main">
+        <img src={HappyPeople} alt="" className="start-flow__image" />
+      </div>
 
       <div className="start-flow__bottom">
         <div className="start-flow__description">
-          <h1>LDIS Live Discussion</h1>
-          <p>Привіт, готовий до найкращого спілкування в своєму житті?</p>
+          <Typography
+            className="start-flow__title"
+            sx={{ ...theme.typography.h1Bold }}
+          >
+            <span className="start-flow__title-part">LDIS</span> Live Discussion
+          </Typography>
+          <Typography variant="body1">
+            Привіт, готовий до найкращого спілкування в своєму житті?
+          </Typography>
         </div>
 
         <div className="start-flow__buttons">
